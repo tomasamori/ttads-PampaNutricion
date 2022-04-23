eaaf6be38c93fa41fca2d186da7b36e6720d1594
AlexisTomas2000 <alexisjosetomas@gmail.com>
2022-04-23 16:19:54 -0300

    Add precio,producto y tipoMascota

---
+++ entities/tipoMascota.js
@@ -0,0 +1,8 @@
+class TipoMascota {
+    constructor(idTipoMascota, nombre, tamañoRaza, edad) {
+        this.idTipoMascota = idTipoMascota;
+        this.nombre = nombre;
+        this.tamañoRaza = tamañoRaza;
+        this.edad = edad;
+    }
+}
