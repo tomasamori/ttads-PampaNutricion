2dae5c2995092a78d8b6df38f585a5fa79fbbbf0
tomasamori <tomasamori15@gmail.com>
2022-04-23 16:06:58 -0300

    se agregan clases

---
+++ entities/cliente.js
@@ -0,0 +1,13 @@
+class Cliente {
+    constructor(idUsuario, usuario, password, cuil, email, fechaNacimiento, direccion, telefono, razonSocial) {
+        this.idUsuario = idUsuario;
+        this.usuario = usuario;
+        this.password = password;
+        this.cuil = cuil;
+        this.email = email;
+        this.fechaNacimiento = fechaNacimiento;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.razonSocial = razonSocial;
+    }
+}
