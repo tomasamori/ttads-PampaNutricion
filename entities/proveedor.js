eb5eb5ae88227a7189c7148c3f57e86f71cfb8f9
JuanCruzV4 <juancruz.vazquez87@hotmail.com>
2022-04-23 16:19:37 -0300

    add class Proveedor

---
+++ entities/proveedor.js
@@ -0,0 +1,10 @@
+class Proveedor {
+    constructor(idProveedor, cuil, cuit, razonSocial, email, telefono) {
+        this.idProveedor = id;
+        this.cuil = cuil;
+        this.cuit = cuit;
+        this.razonSocial = razonSocial;
+        this.email = email;
+        this.telefono = telefono;
+    }
+}
