b28b2640aae6e0f4ffb1a918aec9abac16f01f15
tomasamori <tomas.amori@gmail.com>
2023-03-14 09:55:17 -0300

    Fix list-filter pipe error

--- src/app/pipes/list-filter.pipe.ts
+++ src/app/pipes/list-filter.pipe.ts
@@ -6,6 +6,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ListFilterPipe implements PipeTransform {
 
   transform(value: any, arg: any): any {
+
+    if (!value) return null;
+    if (!arg) return value;
+
     const resultProducts = [];
 
     for (const p of value) {
