cbafac9b75694306fdf4987a92e8e93e6e47baed
matimayor <matias.mayor99@gmail.com>
2024-02-16 15:59:46 -0300

    Update register and login modal

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -11,10 +11,30 @@ import { NgForm } from "@angular/forms";
 export class NavbarComponent implements OnInit {
 
   registrationSuccess = false;
+  errorMessage: string = "";
   
   constructor(public authService: AuthService) { }
 
   ngOnInit(): void {
+  }
+  
+  login(form: NgForm) {
+    this.authService.login(form.value)
+    .subscribe(res => {
+      console.log(res);
+      this.registrationSuccess = true;
+      form.reset();
+    },
+    err => {
+      console.log(err);
+      if (err && err.message) {
+        this.errorMessage = err.error.message;
+      } else {
+        this.errorMessage = 'Se produjo un error desconocido';
+      }
+      this.registrationSuccess = false;
+    }
+    )
   }
   
   register(form: NgForm){
@@ -24,6 +44,14 @@ export class NavbarComponent implements OnInit {
         console.log(res); // acá va a mostrar el token que vuelve del back. habría que guardar el token en el local storage y redirigir al usuario a la home logueado (luego usar ese token para hacer peticiones al back y que el back sepa que el usuario está logueado)
         this.registrationSuccess = true;
       },
-      err => console.log(err)
+      err => {
+        console.log(err);
+        if (err && err.message) {
+          this.errorMessage = err.error.message;
+        } else {
+          this.errorMessage = 'Se produjo un error desconocido';
+        }
+        this.registrationSuccess = false;
+      }
     )}  
     }
